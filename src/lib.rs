921c23333e30dc44f1c672fca00ff600c92936bc
Guy Bedford <gbedford@cloudflare.com>
2026-07-16 14:11:56 -0700

    rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -34,12 +34,12 @@ pub use args::BlockingArgs;
 #[cfg(target_os = "emscripten")]
 mod emscripten;
 #[cfg(target_os = "emscripten")]
-pub use emscripten::{blocking_call, enter_promising, linked};
+pub use emscripten::{blocking_call, enter_promising};
 
 #[cfg(not(target_os = "emscripten"))]
 mod unsupported;
 #[cfg(not(target_os = "emscripten"))]
-pub use unsupported::{blocking_call, enter_promising, linked};
+pub use unsupported::{blocking_call, enter_promising};
 
 #[doc(hidden)]
 pub const fn __em_js_len(s: &str) -> usize {
